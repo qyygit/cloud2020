4cefa67525c43c51e655b7c0c8629037df1b0ae8
Mrs 洋 <tongyang0109@163.com>
2020-11-09 10:38:52 +0800

    订单消费者启动类提交

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: cloud2020
+ * @description: 80微服务订单消费者
+ * @author: Mr.Yang
+ * @create: 2020-11-09 09:28
+ **/
+@SpringBootApplication
+public class OrderMain80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class,args);
+        System.out.println("80 -- 微服务订单消费者启动成功 ");
+    }
+}
