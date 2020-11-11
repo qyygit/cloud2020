55d00d2bbc38ae9a64de727de490d6cde7a05388
Mrs 洋 <tongyang0109@163.com>
2020-11-11 11:19:39 +0800

    订单消费者启动类提交

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -0,0 +1,38 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+
+/**
+ * @program: cloud2020
+ * @description: 80 订单微服务消费者表现层
+ * @author: Mr.Yang
+ * @create: 2020-11-09 09:49
+ **/
+@RestController
+@Slf4j
+public class OrderController {
+
+    private static final String PAYMENT_URL = "http://localhost:8001";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/create")
+    public CommonResult<Payment> create(Payment payment) {
+        log.info("80订单微服务消费者 ---- 消费成功");
+        return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
+                                                                     log.info("80订单微服务消费者 ---- 消费成功");
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+}
